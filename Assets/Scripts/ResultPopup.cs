df855b32ff4a72e70829e327f1b821a77e7d17a3
IHyang <icek314@gmail.com>
2025-06-05 21:44:33 +0900

    하이스코어 시스템 추가 데이터 저장 추가

--- Assets/Scripts/ResultPopup.cs
+++ Assets/Scripts/ResultPopup.cs
@@ -8,7 +8,7 @@ public class ResultPopup : MonoBehaviour
 {
     public TextMeshProUGUI titleLabel;
     public TextMeshProUGUI scoreLabel;
-
+    public GameObject highScorePopup;    
 
     private void OnEnable()
     {
@@ -16,14 +16,79 @@ public class ResultPopup : MonoBehaviour
 
         if(GameManager.Instance.isCleared)
         {
+
+            SaveHighScore();
+
+            /*float highScore = PlayerPrefs.GetFloat("HighScore", 0);
+
+            if(highScore < GameManager.Instance.timeLimit)
+            {
+                highScoreLabel.SetActive(true);
+
+                PlayerPrefs.SetFloat("HighScore", GameManager.Instance.timeLimit);
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                highScoreLabel.SetActive(false);
+            }*/
+
+            
+
             titleLabel.text = "Cleared!";
             scoreLabel.text = GameManager.Instance.timeLimit.ToString("#.##");
         }
         else
         {
+            
             titleLabel.text = "GameOver";
             scoreLabel.text = "";
         }
+    }
+
+    void SaveHighScore()
+    {
+        float score = GameManager.Instance.timeLimit;
+        string currentScoreString = score.ToString("#.###");
+
+        string savedScoreString = PlayerPrefs.GetString("HighScores","");
+
+        if (savedScoreString == "")
+        {
+            PlayerPrefs.SetString("HighScores", currentScoreString);
+        }
+        else
+        {
+            string[] scoreArray = savedScoreString.Split(",");
+            List<string> scoreList = new List<string>(scoreArray);
+
+            for (int i=0; i < scoreList.Count; i ++)
+            {
+                float savedScore = float.Parse(scoreList[i]);
+                if (savedScore < score)
+                {
+                    scoreList.Insert(i, currentScoreString);
+                    break;
+                }
+            }
+
+            if(scoreArray.Length == scoreList.Count)
+            {
+                scoreList.Add(currentScoreString);
+            }
+
+            if (scoreList.Count>10)
+            {
+                scoreList.RemoveAt(10);
+            }
+
+            string result = string.Join(",", scoreList);
+            PlayerPrefs.SetString("HighScores", result);
+
+        }
+
+        
+        PlayerPrefs.Save();
     }
 
     public void PlayAgainPressed()
@@ -31,5 +96,10 @@ public class ResultPopup : MonoBehaviour
         Time.timeScale = 1;
 
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void HighScorePressed()
+    {
+        highScorePopup.SetActive(true);
     }
 }
